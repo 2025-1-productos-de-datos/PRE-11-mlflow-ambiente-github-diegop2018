335d841113724cf13ea0fd3db3492874c9d837ba
diegop2018 <36285502+diegop2018@users.noreply.github.com>
2025-05-24 12:31:48 -0500

    Test3

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -8,12 +8,17 @@ warnings.filterwarnings("ignore")
 
 
 def test_01():
-
     # Test if the homework script runs without errors
     try:
         subprocess.run(
             [
-                "run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5",
+                "mlflow",
+                "run",
+                "https://github.com/jdvelasq/mlflow-wine-quality.git",
+                "-e",
+                "knn",
+                "-P",
+                "n_neighbors=5",
             ],
             check=True,
         )
